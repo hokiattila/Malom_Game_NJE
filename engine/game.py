c91ce942f0b1e06459ecd2019e74e24560790f71
Hoki Attila <hoki.attila@gmail.com>
2024-10-02 15:00:38 +0200

    Rearranged log folder and added log_game() function

--- engine/game.py
+++ engine/game.py
@@ -1,6 +1,11 @@
+import os.path
+from datetime import datetime
+from random import getrandbits
 
 class Game:
-    def __init__(self):
+    def __init__(self, mode="pvp"):
+        self.game_id = getrandbits(128)
+        self.time = datetime.now()
         self.board = [str(x) for x in range(0,24)]
         self.mills = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11), (12, 13, 14), (15, 16, 17), (18, 19, 20), (21, 22, 23),(0, 9, 21), (3, 10, 18), (6, 11, 15), (1, 4, 7), (16, 19, 22), (8, 12, 17), (5, 13, 20), (2, 14, 23))
 
@@ -22,7 +27,24 @@ class Game:
             '                     {}--------------{}---------------{}\n'.format(self.adjust(self.board[0]), self.adjust(self.board[1]), self.adjust(self.board[2]), self.adjust(self.board[3]), self.adjust(self.board[4]), self.adjust(self.board[5]), self.adjust(self.board[6]),
                 self.adjust(self.board[7]),self.adjust(self.board[8]), self.adjust(self.board[9]), self.adjust(self.board[10]), self.adjust(self.board[11]), self.adjust(self.board[12]), self.adjust(self.board[13]), self.adjust(self.board[14]),self.adjust(self.board[15]),self.adjust(self.board[16]), self.adjust(self.board[17]), self.adjust(self.board[18]), self.adjust(self.board[19]), self.adjust(self.board[20]), self.adjust(self.board[21]), self.adjust(self.board[22]),
                 self.adjust(self.board[23])))
+        print(self.game_id)
+        print(self.time)
         print()
+
+    def log_game(self):
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        log_dir = os.path.join(project_root, "log")
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_file_path = os.path.join(log_dir, f"{self.game_id}.txt")
+        if not os.path.exists(log_file_path):
+            with open(log_file_path, mode="w") as f:
+                f.write(f"Game ID: {self.game_id}\n")
+                f.write(f"Time: {self.time}\n")
+        else:
+            return
+
+
 
     @staticmethod
     def adjust(char):
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from engine.game import Game
 if __name__ == '__main__':
     new_game = Game()
     new_game.print_board()
+    new_game.log_game()
